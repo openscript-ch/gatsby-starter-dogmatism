b838ec3df850a3c8368597fea2406567cd43943f
Robin <1105080+openscript@users.noreply.github.com>
2021-11-06 10:57:13 +0000

    Fix filename parsing

--- plugins/gatsby-plugin-i18n-l10n/src/onCreateNode/translateNode.ts
+++ plugins/gatsby-plugin-i18n-l10n/src/onCreateNode/translateNode.ts
@@ -11,8 +11,8 @@ export const translateNode: onCreateNode = async ({ getNode, node, actions }, op
 
   if ((node.internal.type === 'MarkdownRemark' || node.internal.type === 'Mdx') && node.parent && options) {
     const fileSystemNode = getNode(node.parent);
-    const { name, relativeDirectory, absolutePath } = fileSystemNode as FileSystemNode;
-    const { filename, estimatedLocale } = parseFilename(name, options.defaultLocale);
+    const { name, ext, relativeDirectory, absolutePath } = fileSystemNode as FileSystemNode;
+    const { filename, estimatedLocale } = parseFilename(`${name}${ext}`, options.defaultLocale);
     const { title } = node['frontmatter'] as { title?: string };
     const locale = findLocale(estimatedLocale, options);
     const { slug, kind, filepath } = translatePath(filename, relativeDirectory, locale, options, title);
